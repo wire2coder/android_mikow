2d89d16111c561ea2748b45d17f31585768447f1
Andy Murph <murph@email.com>
2017-11-19 08:04:42 -0500

    Added arrays of words for number

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -18,7 +18,11 @@ package com.example.android.miwok;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,13 +32,79 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
-    }
 
-    public void openNumbersList(View view) {
-        // make new intent
-        Intent i = new Intent(this, NumbersActivity.class);
-        startActivity(i);
-    }
+        // Find the View that shows the numbers category
+        TextView numbers = (TextView) findViewById(R.id.numbers);
+        TextView family = (TextView) findViewById(R.id.family);
+        TextView colors = (TextView) findViewById(R.id.colors);
+        TextView phrase = (TextView) findViewById(R.id.phrases);
+
+        // Set a click listener on numbers view
+        numbers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(view.getContext(), "Open the list of numbers", Toast.LENGTH_SHORT).show();
+
+                // create a new Intent
+                Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class  );
+                startActivity(numbersIntent);
+            }
+        });
+
+        // Set a click listener on family view
+        family.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(view.getContext(), "Open the family list of words", Toast.LENGTH_SHORT).show();
+
+                Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+                startActivity(familyIntent);
+            }
+        });
+
+        colors.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(view.getContext(), "Open list of words for color", Toast.LENGTH_SHORT).show();
+
+                    Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+                    startActivity(colorsIntent);
+            }
+        });
+
+        phrase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Toast.makeText(view.getContext(), "Open list of words for phrase", Toast.LENGTH_SHORT).show();
+
+                Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+                startActivity(phrasesIntent);
+            }
+        });
+
+        String [] words = new String[10];
+
+        words[0] = "one";
+        words[1] = "two";
+        words[2] = "three";
+        words[3] = "four";
+        words[4] = "five";
+        words[5] = "six";
+        words[6] = "seven";
+        words[7] = "eight";
+        words[8] = "nine";
+        words[9] = "ten";
+
+        Log.v("NumberActivity", "Word at index 0: " + words[0]);
+        Log.v("NumberActivity", "Word at index 1: " + words[1]);
+
+    } // onCreate
+
+//    public void openNumbersList(View view) {
+//        // make new intent
+//        Intent i = new Intent(this, NumbersActivity.class);
+//        startActivity(i);
+//    }
 
 } // class MainActivity
 
