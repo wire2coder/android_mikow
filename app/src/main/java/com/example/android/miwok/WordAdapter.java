1f638b945fe63c655d3525680a590b121f87e4e4
Andy Murph <murph@email.com>
2017-11-22 09:24:05 -0500

    look at note

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -8,7 +8,9 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 public class WordAdapter extends ArrayAdapter<Word> {
-    @NonNull
+
+
+    // TODO
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         return super.getView(position, convertView, parent);
